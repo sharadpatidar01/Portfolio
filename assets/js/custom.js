6774471ddf4cd91a412f167ae048ee2808dd256b
Sharad <patidarsharad01@gmail.com>
2021-08-20 02:19:40 +0530

    Insert commit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -132,7 +132,8 @@ var TxtRotate = function(el, toRotate, period) {
         new TxtRotate(elements[i], JSON.parse(toRotate), period);
       }
     }
-    // INJECT CSS
+
+    //INJECT CSS
     var css = document.createElement("style");
     css.type = "text/css";
     css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
